002897865d9f0b32f06f140389bbb9830abbfa0a
KevenGoulart <kevengoulartmm@gmail.com>
2024-11-21 19:58:11 -0300

    commit

---
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { CalendarStep } from './CalendarStep'
+import { ConfirmForm } from './ConfirmStep/styles'
+
+export function ScheduleForm() {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>()
+
+  function handleClearSelectedDateTime() {
+    setSelectedDateTime(null)
+  }
+
+  if (selectedDateTime) {
+    return (
+      <ConfirmForm
+        schedulingDate={selectedDateTime}
+        onCancelConfirmation={handleClearSelectedDateTime}
+      />
+    )
+  }
+
+  return <CalendarStep onSelectDateTime={setSelectedDateTime} />
+}
